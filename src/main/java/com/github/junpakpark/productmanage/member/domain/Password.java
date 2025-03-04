7ae6ea1cf3906af3a724fcb304329499205753a2
junpakPark <112045553+junpakPark@users.noreply.github.com>
2025-03-04 16:59:15 +0900

    feat: Password equals and hashcode 추가

--- src/main/java/com/github/junpakpark/productmanage/member/domain/Password.java
+++ src/main/java/com/github/junpakpark/productmanage/member/domain/Password.java
@@ -3,6 +3,7 @@ package com.github.junpakpark.productmanage.member.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,4 +22,18 @@ public class Password {
         this.lastPasswordChangedAt = LocalDateTime.now();
     }
 
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Password password1 = (Password) o;
+        return Objects.equals(password, password1.password) && Objects.equals(lastPasswordChangedAt,
+                password1.lastPasswordChangedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(password, lastPasswordChangedAt);
+    }
 }
