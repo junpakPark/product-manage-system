66941785af19fc85a0a6b69a9c7b46d4b16f14f7
junpakPark <112045553+junpakPark@users.noreply.github.com>
2025-03-05 20:30:15 +0900

    feat: JWT 기반 JPA auditing  구현

--- src/main/java/com/github/junpakpark/productmanage/common/config/JpaConfig.java
+++ src/main/java/com/github/junpakpark/productmanage/common/config/JpaConfig.java
@@ -1,10 +1,21 @@
 package com.github.junpakpark.productmanage.common.config;
 
+import com.github.junpakpark.productmanage.common.infrastructure.audit.JwtAuditAware;
+import com.github.junpakpark.productmanage.common.security.application.port.out.token.TokenValidator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Slf4j
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
+
+    @Bean
+    public AuditorAware<Long> auditorProvider(final TokenValidator tokenValidator) {
+        return new JwtAuditAware(tokenValidator);
+    }
 
 }
---
+++ src/main/java/com/github/junpakpark/productmanage/common/domain/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.github.junpakpark.productmanage.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity extends BaseTimeEntity{
+
+    @CreatedBy
+    @Column(updatable = false)
+    private Long createdBy;
+
+    @LastModifiedBy
+    private Long updatedBy;
+
+    private LocalDateTime deletedAt;
+
+    public void softDelete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public void restore() {
+        this.deletedAt = null;
+    }
+
+}
