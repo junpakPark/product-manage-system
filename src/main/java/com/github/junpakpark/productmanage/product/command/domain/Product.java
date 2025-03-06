d22211b5e509386d027a0867d009ae1480f9a983
junpakPark <112045553+junpakPark@users.noreply.github.com>
2025-03-06 21:23:26 +0900

    feat: Product에 ProductOptions 일급컬렉션 추가

--- src/main/java/com/github/junpakpark/productmanage/product/command/domain/Product.java
+++ src/main/java/com/github/junpakpark/productmanage/product/command/domain/Product.java
@@ -1,6 +1,8 @@
 package com.github.junpakpark.productmanage.product.command.domain;
 
 import com.github.junpakpark.productmanage.common.domain.BaseEntity;
+import com.github.junpakpark.productmanage.product.command.domain.option.ProductOption;
+import com.github.junpakpark.productmanage.product.command.domain.option.ProductOptions;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -24,23 +26,39 @@ public class Product extends BaseEntity {
     private ProductInfo productInfo;
     @Column(nullable = false)
     private Long memberId;
+    @Embedded
+    private ProductOptions productOptions;
 
     public Product(final ProductInfo productInfo, final Long memberId) {
         validateProductInfo(productInfo);
         validateMember(memberId);
         this.productInfo = productInfo;
         this.memberId = memberId;
+        this.productOptions = new ProductOptions();
     }
 
     public void validateOwner(final Long ownerId) {
         if (!Objects.equals(this.memberId, ownerId)) {
-            throw new IllegalArgumentException("Owner id mismatch");
+            throw new IllegalArgumentException("상품 판매자 정보가 일치하지 않습니다. (productId= %d)".formatted(this.id));
         }
     }
 
     public void updateInfo(final ProductInfo productInfo) {
         validateProductInfo(productInfo);
         this.productInfo = productInfo;
+    }
+
+    public void addOption(final ProductOption productOption) {
+        productOption.associatedWith(this);
+        this.productOptions.add(productOption);
+    }
+
+    public void removeOption(final Long optionId) {
+        this.productOptions.removeById(optionId);
+    }
+
+    public void updateOption(final Long optionId, final ProductOption updateOption) {
+        this.productOptions.update(optionId, updateOption);
     }
 
     private void validateProductInfo(final ProductInfo productInfo) {
